f4033c5a78517b60cab3eec3027eaf680dea8d24
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-10-21 17:07:08 -0400

    Step created

--- src/main/java/com/venkatesh/springbatch/config/BatchConfig.java
+++ src/main/java/com/venkatesh/springbatch/config/BatchConfig.java
@@ -4,6 +4,9 @@ import com.venkatesh.springbatch.entity.Employee;
 import com.venkatesh.springbatch.processor.EmployeeProcessor;
 import com.venkatesh.springbatch.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +25,10 @@ public class BatchConfig {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
 
     @Bean
     public FlatFileItemReader<Employee> itemReader(){
@@ -62,5 +70,16 @@ public class BatchConfig {
         itemWriter.setRepository(employeeRepository);
         itemWriter.setMethodName("save");
         return itemWriter;
+    }
+
+    //Step
+    @Bean
+    public Step step1(){
+        return new StepBuilder("csvStep1",jobRepository)
+                .<Employee,Employee>chunk(10,platformTransactionManager)
+                .reader(itemReader())
+                .processor(employeeProcessor())
+                .writer(itemWriter())
+                .build();
     }
 }
